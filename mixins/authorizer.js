2f941dc4e9d9611fe8e9a05b3e9b8e52bbd24368
Daniele Draganti <daniele.draganti@vegacyl.com>
2019-11-07 18:26:46 +0000

    Improved authenticator function with whitelist

--- mixins/authorizer.js
+++ mixins/authorizer.js
@@ -5,15 +5,16 @@ module.exports = function Authorizer(opts) {
   return {
     settings: {
       authorizer: {
-        //whitelist: [],
-        //blacklist: []
+        //whitelist: []
       }
     },
 
     hooks: {
       before: {
+        if (whitelist && whitelist.indexOf && whitelist.indexOf(ctx.action.name) > -1)
+          return;
         '*': async function(ctx) {
-          if (!ctx.meta.role || !ctx.meta.role.actions || ctx.meta.role.actions.indexOf(ctx.action.name) < 0)
+          if (!ctx.meta.role || !ctx.meta.role.actions || !ctx.meta.role.actions.indexOf || ctx.meta.role.actions.indexOf(ctx.action.name) < 0)
             throw new MoleculerError('Action not authorized.', 403, 'E_NOT_AUTHORIZED');
         }
       }
