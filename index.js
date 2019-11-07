87f190919b3b6b5fac7904104d70cf8fb684a5a5
Daniele Draganti <daniele.draganti@vegacyl.com>
2019-11-07 17:45:16 +0000

    Added middleware to handle authorization

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 "use strict";
 
 module.exports = {
-  middlewares: {
-    InterNamespace: require("./middlewares/inter-namespace.js"),
-    K8sHealthCheck: require("./middlewares/k8s-health-check.js")
-  }
+  middlewares: require("./middlewares");
 }
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 module.exports = {
+  Authorizer: require("./authorizer.js"),
   InterNamespace: require("./inter-namespace.js"),
   K8sHealthCheck: require("./k8s-health-check.js")
 }
