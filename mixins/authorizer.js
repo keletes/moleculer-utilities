a88cb2d008ea570de38ef31b0f43f2465b58c032
Daniele Draganti <daniele.draganti@vegacyl.com>
2019-11-07 21:00:50 +0000

    Logger

--- mixins/authorizer.js
+++ mixins/authorizer.js
@@ -12,6 +12,9 @@ module.exports = function Authorizer(opts) {
     hooks: {
       before: {
         '*': async function(ctx) {
+
+          console.log(ctx.action);
+
           if (this.settings.authorizer &&
               this.settings.authorizer.whitelist &&
               this.settings.authorizer.whitelist.indexOf &&
