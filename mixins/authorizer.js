a30bacf294da2708f9eafc8281c5bf470f75233a
Daniele Draganti <daniele.draganti@vegacyl.com>
2019-11-07 18:22:54 +0000

    Fixed authenticator function

--- mixins/authorizer.js
+++ mixins/authorizer.js
@@ -13,7 +13,7 @@ module.exports = function Authorizer(opts) {
     hooks: {
       before: {
         '*': async function(ctx) {
-          if (ctx.meta.role.actions.indexOf(ctx.action.name) < 0)
+          if (!ctx.meta.role || !ctx.meta.role.actions || ctx.meta.role.actions.indexOf(ctx.action.name) < 0)
             throw new MoleculerError('Action not authorized.', 403, 'E_NOT_AUTHORIZED');
         }
       }
