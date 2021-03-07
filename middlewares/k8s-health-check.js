74c06647f76f609de766e80967145b374bd56ec2
Daniele Draganti <daniele.d@keletes.com>
2021-03-07 13:54:58 +0000

    Updated health check to handle liveness and readiness probes

--- middlewares/k8s-health-check.js
+++ middlewares/k8s-health-check.js
@@ -1,53 +1,91 @@
 "use strict";
+
 const _ = require("lodash");
 const http = require("http");
-module.exports = function(opts) {
+
+module.exports = function (opts) {
   opts = _.defaultsDeep(opts, {
     port: 3001,
-    path: "/health"
+    readiness: {
+      path: "/ready",
+    },
+    liveness: {
+      path: "/live",
+    },
   });
-  let state = "starting";
+
+  let state = "down";
   let server;
+
   function handler(req, res) {
-    if (req.url == opts.path) {
+    if (req.url == opts.readiness.path || req.url == opts.liveness.path) {
       const resHeader = {
-      "Content-Type": "application/json; charset=utf-8"
+        "Content-Type": "application/json; charset=utf-8",
       };
+
       const content = {
         state,
         uptime: process.uptime(),
-        timestamp: Date.now()
+        timestamp: Date.now(),
       };
-      res.writeHead(state == "up" ? 200 : 503, resHeader);
+
+      if (req.url == opts.readiness.path) {
+        // Readiness if the broker started successfully.
+        // https://kubernetes.io/docs/tasks/configure-pod-container/configure-liveness-readiness-startup-probes/#define-readiness-probes
+        res.writeHead(state == "up" ? 200 : 503, resHeader);
+      } else {
+        // Liveness if the broker is not stopped.
+        // https://kubernetes.io/docs/tasks/configure-pod-container/configure-liveness-readiness-startup-probes/#define-a-liveness-command
+        res.writeHead(state != "down" ? 200 : 503, resHeader);
+      }
+
       res.end(JSON.stringify(content, null, 2));
     } else {
       res.writeHead(404, http.STATUS_CODES[404], {});
       res.end();
     }
   }
+
   return {
     created(broker) {
+      state = "starting";
+
       server = http.createServer(handler);
       server.on("request", handler);
-      server.listen(opts.port, err => {
+      server.listen(opts.port, (err) => {
         if (err) {
-          return broker.logger.error("Unable to start health-check server", err);
+          return broker.logger.error(
+            "Unable to start health-check server",
+            err
+          );
         }
-        broker.logger.info(`Health-check server listening on http://0.0.0.0:${opts.port}${opts.path} address.`);
+
+        broker.logger.info("");
+        broker.logger.info("K8s health-check server listening on");
+        broker.logger.info(
+          `    http://localhost:${opts.port}${opts.readiness.path}`
+        );
+        broker.logger.info(
+          `    http://localhost:${opts.port}${opts.liveness.path}`
+        );
+        broker.logger.info("");
       });
     },
+
     // After broker started
     started(broker) {
       state = "up";
     },
+
     // Before broker stopping
     stopping(broker) {
       state = "stopping";
     },
+
     // After broker stopped
     stopped(broker) {
       state = "down";
       server.close();
-    }
+    },
   };
-}
+};
