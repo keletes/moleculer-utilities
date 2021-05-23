f0333c79aeaef0fdacb13e5e7388949c5aa32148
Daniele Draganti <daniele.d@keletes.com>
2021-05-23 13:44:03 +0200

    Using ES7 optional chaining

--- mixins/authorizer.js
+++ mixins/authorizer.js
@@ -6,29 +6,28 @@ module.exports = function Authorizer(opts) {
     settings: {
       authorizer: {
         //whitelist: []
-      }
+      },
     },
 
     hooks: {
       before: {
-        '*': async function(ctx) {
-          if (this.settings.authorizer &&
-              this.settings.authorizer.whitelist &&
-              this.settings.authorizer.whitelist.indexOf &&
-              this.settings.authorizer.whitelist.indexOf(ctx.action.name) > -1
+        "*": async function (ctx) {
+          if (
+            this.settings?.authorizer?.whitelist?.indexOf?.(ctx.action.name) >
+            -1
           )
             return;
 
           const authorizer = ctx.meta.authorizer;
 
-          if (!authorizer ||
-              !authorizer.actions ||
-              !authorizer.actions.indexOf ||
-               authorizer.actions.indexOf(ctx.action.name) < 0
-          )
-            throw new MoleculerError('Action not authorized.', 403, 'E_NOT_AUTHORIZED');
-        }
-      }
-    }
-  }
+          if (authorizer.actions?.indexOf?.(ctx.action.name) < 0)
+            throw new MoleculerError(
+              "Action not authorized.",
+              403,
+              "E_NOT_AUTHORIZED"
+            );
+        },
+      },
+    },
+  };
 };
